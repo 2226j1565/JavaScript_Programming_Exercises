51ec9d8a59b58e3df43fc73d251da32bb116f714
2226j1565 <144420321+2226j1565@users.noreply.github.com>
2024-07-03 09:26:55 +0530

    Update Ch2_pgm_06.js

--- 02_JS_variables/Ch2_pgm_06.js
+++ 02_JS_variables/Ch2_pgm_06.js
@@ -1,5 +1,3 @@
-// Declaring and assigning in two steps
-
 var playerName;
 var locationName;
 
@@ -7,17 +5,6 @@ playerName = "Lionel Messi";
 locationName = "Argentina";
 
 console.log(playerName + " is in " + locationName);
-
-
-
-/* Further Adventures
- *
- * 1) Declare a score variable.
- *
- * 2) Assign it a value.
- *
- * 3) Display an extra message on the console,
- *    by combining variables and strings, to say
- *    something like "Lionel Messi has a score of 100"
- *
- */
+var score;
+score = 100;
+console.log(playerName + " has a score of " + score);
