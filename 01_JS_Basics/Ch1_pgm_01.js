6ece01071142b0a292449ebb2e9c2f03891e15bc
2226j1565 <144420321+2226j1565@users.noreply.github.com>
2024-07-03 09:22:33 +0530

    Update Ch1_pgm_01.js

--- 01_JS_Basics/Ch1_pgm_01.js
+++ 01_JS_Basics/Ch1_pgm_01.js
@@ -1,18 +1,4 @@
-// Using console.log to display information
-
+<script>
 console.log("Hello World!");
-
-
-/* Further Adventures
- *
- * 1) Change the text between the parentheses.
- *
- * 2) Click Run on the console to run the program again.
- *
- * 3) Add your own console.log statement
- *    to the program, so there are two.
- *    Don't forget the semicolon.
- *
- * 4) Click Run.
- *
- */
+console.log("This is javascript");
+</script>
