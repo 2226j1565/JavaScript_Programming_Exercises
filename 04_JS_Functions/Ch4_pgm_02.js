8950d51e8697d0858a8e07e35534363d8422e4fa
2226j1565 <144420321+2226j1565@users.noreply.github.com>
2024-07-03 12:38:04 +0530

    Update Ch4_pgm_02.js

--- 04_JS_Functions/Ch4_pgm_02.js
+++ 04_JS_Functions/Ch4_pgm_02.js
@@ -40,17 +40,86 @@ console.log("Actors: " + movie3.actors);
 console.log("Directors: " + movie3.directors);
 console.log("------------------------------");
 
+var movie1, movie2, movie3, movie4;
 
+movie1 = {
+  title: "Inside Out",
+  actors: "Amy Poehler, Bill Hader",
+  directors: "Pete Docter, Ronaldo Del Carmen"
+};
 
-/* Further Adventures
- *
- * 1) Add a fourth movie and display its info
- *
- * 2) All the movie info is in one big block on the console.
- *    Change the code to space out the different movies.
- *
- * 3) Create objects to represent three calendar events
- *
- * 4) Display info from the three events on the console.
- *
- */
+movie2 = {
+  title: "Spectre",
+  actors: "Daniel Craig, Christoph Waltz",
+  directors: "Sam Mendes"
+};
+
+movie3 = {
+  title: "Star Wars: Episode VII - The Force Awakens",
+  actors: "Harrison Ford, Mark Hamill, Carrie Fisher",
+  directors: "J.J. Abrams"
+};
+
+movie4 = {
+  title: "Inception",
+  actors: "Leonardo DiCaprio, Joseph Gordon-Levitt, Ellen Page",
+  directors: "Christopher Nolan"
+};
+
+console.log("Movie information for " + movie1.title);
+console.log("------------------------------");
+console.log("Actors: " + movie1.actors);
+console.log("Directors: " + movie1.directors);
+console.log("------------------------------");
+
+console.log("Movie information for " + movie2.title);
+console.log("------------------------------");
+console.log("Actors: " + movie2.actors);
+console.log("Directors: " + movie2.directors);
+console.log("------------------------------");
+
+console.log("Movie information for " + movie3.title);
+console.log("------------------------------");
+console.log("Actors: " + movie3.actors);
+console.log("Directors: " + movie3.directors);
+console.log("------------------------------");
+
+console.log("Movie information for " + movie4.title);
+console.log("------------------------------");
+console.log("Actors: " + movie4.actors);
+console.log("Directors: " + movie4.directors);
+console.log("------------------------------");
+
+var event1 = {
+  title: "Meeting with Client",
+  date: "July 5, 2024",
+  location: "123 Main St, Anytown"
+};
+
+var event2 = {
+  title: "Team Lunch",
+  date: "July 7, 2024",
+  location: "Café Bistro, Downtown"
+};
+
+var event3 = {
+  title: "Conference Presentation",
+  date: "July 10, 2024",
+  location: "Convention Center, City Center"
+};
+
+// Displaying calendar event information
+console.log("Calendar Event: " + event1.title);
+console.log("Date: " + event1.date);
+console.log("Location: " + event1.location);
+console.log("------------------------------");
+
+console.log("Calendar Event: " + event2.title);
+console.log("Date: " + event2.date);
+console.log("Location: " + event2.location);
+console.log("------------------------------");
+
+console.log("Calendar Event: " + event3.title);
+console.log("Date: " + event3.date);
+console.log("Location: " + event3.location);
+console.log("------------------------------");
