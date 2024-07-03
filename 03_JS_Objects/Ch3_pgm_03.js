3666076bfe59a77251d816de9f37ecae03d39b13
2226j1565 <144420321+2226j1565@users.noreply.github.com>
2024-07-03 09:39:25 +0530

    Update Ch3_pgm_03.js

--- 03_JS_Objects/Ch3_pgm_03.js
+++ 03_JS_Objects/Ch3_pgm_03.js
@@ -1,25 +1,16 @@
 // A book as an object
-
 var book;
-
 book = {
-    title : "The Hobbit",
-    author : "J. R. R. Tolkien",
-    published : 1937
+    title: "The Hobbit",
+    author: "J. R. R. Tolkien",
+    published: 1937
 };
-
 console.log(book);
-
-
-
-/* Further Adventures
- *
- * 1) Change the title of book to
- *    "The Hobbit, or There and Back Again".
- *    Run the program again.
- *
- * 2) Try creating a book2 object.
- *
- * 3) Log book2 to the console.
- *
- */
+book.title = "The Hobbit, or There and Back Again";
+console.log(book);
+var book2 = {
+    title: "1984",
+    author: "George Orwell",
+    published: 1949
+};
+console.log(book2);
