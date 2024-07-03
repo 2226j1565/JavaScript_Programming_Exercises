34067ef4876846e93044fdca05c691adde3ca990
2226j1565 <144420321+2226j1565@users.noreply.github.com>
2024-07-03 09:44:09 +0530

    Update Ch3_pgm_07.js

--- 03_JS_Objects/Ch3_pgm_07.js
+++ 03_JS_Objects/Ch3_pgm_07.js
@@ -2,26 +2,16 @@
 
 var book1;
 var book2;
-
 book1 = {
-    title : "The Hobbit",
-    author : "J. R. R. Tolkien"
+    title   : "The Hobbit",
+    author  : "J. R. R. Tolkien",
+    publishedYear: 1937
 };
 
 book2 = {
-	title : "Northern Lights",
-	author : "Philip Pullman"
+    title   : "Northern Lights",
+    author  : "Philip Pullman",
+    publishedYear: 1995
 };
-
-
-
-/* Further Adventures
- *
- * 1) Add a third property to each book.
- *    Don't forget the commas.
- *
- * 2) Add spaces so that the colons line up.
- *
- * 3) Log both objects to the console.
- *
- */
+console.log(book1);
+console.log(book2);
