eb9a85d922ccb4917000355b23e7abac4a102ef7
2226j1565 <144420321+2226j1565@users.noreply.github.com>
2024-07-03 12:35:07 +0530

    Update Ch4_pgm_01.js

--- 04_JS_Functions/Ch4_pgm_01.js
+++ 04_JS_Functions/Ch4_pgm_01.js
@@ -14,14 +14,30 @@ console.log("Actors: " + movie1.actors);
 console.log("Directors: " + movie1.directors);
 console.log("------------------------------");
 
+var movie2 = {
+  title: "The Dark Knight",
+  actors: "Christian Bale, Heath Ledger",
+  directors: "Christopher Nolan"
+};
 
+console.log("Movie information for " + movie2.title);
+console.log("------------------------------");
+console.log("Actors: " + movie2.actors);
+console.log("Directors: " + movie2.directors);
+console.log("------------------------------");
 
-/* Further Adventures
- *
- * 1) Add a second movie and display the same info for it.
- *
- * 2) Create an object to represent a blog post.
- *
- * 3) Write code to display info about the blog post.
- *
- */
+var blogPost = {
+  title: "Introduction to JavaScript",
+  author: "John Doe",
+  content: "JavaScript is a programming language...",
+  published: "July 1, 2024"
+};
+
+console.log("Blog Post: " + blogPost.title);
+console.log("------------------------------");
+console.log("Author: " + blogPost.author);
+console.log("Published Date: " + blogPost.published);
+console.log("------------------------------");
+console.log("Content:");
+console.log(blogPost.content);
+console.log("------------------------------");
