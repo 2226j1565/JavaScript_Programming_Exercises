32c593122cc300c1ad180e775c116652a189da18
2226j1565 <144420321+2226j1565@users.noreply.github.com>
2024-07-03 09:53:01 +0530

    Update Ch3_pgm_12.js

--- 03_JS_Objects/Ch3_pgm_12.js
+++ 03_JS_Objects/Ch3_pgm_12.js
@@ -1,19 +1,22 @@
 // A blog post
 
 var post = {
-	id : 1,
-	title : "My Crazy Space Adventure",
-	author : "Philae",
-	created : "2015-06-21",
-	body : "You will not believe where I just woke up!! Only on a comet..."
+    id: 1,
+    title: "My Crazy Space Adventure",
+    author: "Philae",
+    created: "2015-06-21",
+    body: "You will not believe where I just woke up!! Only on a comet..."
 };
-
-
-
-/* Further Adventures
- *
- * 1) Display some of the properties.
- *
- * 2) Add a second post.
- *
- */
+console.log("Post Title: " + post.title);
+console.log("Author: " + post.author);
+console.log("Created Date: " + post.created);
+var post2 = {
+    id: 2,
+    title: "Exploring Mars",
+    author: "Curiosity Rover",
+    created: "2020-03-15",
+    body: "Today's findings on Mars are astonishing..."
+};
+console.log("\nSecond Post Title: " + post2.title);
+console.log("Author: " + post2.author);
+console.log("Created Date: " + post2.created);
