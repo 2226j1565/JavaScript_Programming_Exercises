79479098bd6b83398e197bc37e8fd5aa569040be
2226j1565 <144420321+2226j1565@users.noreply.github.com>
2024-07-03 09:46:27 +0530

    Update Ch3_pgm_09.js

--- 03_JS_Objects/Ch3_pgm_09.js
+++ 03_JS_Objects/Ch3_pgm_09.js
@@ -2,31 +2,20 @@
 
 var book1;
 var book2;
-
 book1 = {
-	title: "The Hobbit",
-	author: "J. R. R. Tolkien"
+    title: "The Hobbit",
+    author: "J. R. R. Tolkien"
 };
-
 book2 = {
-	title: "Northern Lights",
-	author: "Philip Pullman"
+    title: "Northern Lights",
+    author: "Philip Pullman"
 };
-
 console.log(book1.title + " by " + book1.author);
 console.log(book2.title + " by " + book2.author);
-
-
-
-/* Further Adventures
- *
- * 1) Add a third book.
- *
- * 2) Log its details to the console.
- *
- * 3) Add a third property.
- *
- * 4) Update the messages logged to include the
- *    new property.
- *
- */
+var book3 = {
+    title: "To Kill a Mockingbird",
+    author: "Harper Lee"
+};
+console.log(book3.title + " by " + book3.author);
+book3.publishedYear = 1960;
+console.log(book3.title + " by " + book3.author + ", published in " + book3.publishedYear);
