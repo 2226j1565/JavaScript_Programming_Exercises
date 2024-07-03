5574d8b3d1330be769319bced7d658e6d0f17360
2226j1565 <144420321+2226j1565@users.noreply.github.com>
2024-07-03 09:32:19 +0530

    Update Ch2_pgm_09.js

--- 02_JS_variables/Ch2_pgm_09.js
+++ 02_JS_variables/Ch2_pgm_09.js
@@ -5,25 +5,9 @@ var $noProblemHere;
 var _underscore56;
 var StartWithCapital;
 var z5;
-
+// Invalid variable names causing errors
 var 999;
 var 39Steps;
 var &nope;
 var single words only;
 var yield;
-
-
-
-/* Further Adventures
- *
- * 1) Take a look at the errors in the
- *    error section of Console below.
- *
- * 2) Click Run in the console
- *    and see what errors appear there.
- *
- * 3) Delete the second block of variables
- *    one by one from the code.
- *    See how the errors change.
- *
- */
