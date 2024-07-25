074571215a28eb840cc47205349abb979050a1e7
2226j1565 <144420321+2226j1565@users.noreply.github.com>
2024-07-25 22:41:56 +0530

    Update Ch5_pgm_08.js

--- 05_JS_Arguments/Ch5_pgm_08.js
+++ 05_JS_Arguments/Ch5_pgm_08.js
@@ -1,11 +1,9 @@
-// Displaying a player's name via an object property
-
 var player1;
 var player2;
 var showPlayerName;
 
 showPlayerName = function (playerName) {
-    console.log(playerName);
+    console.log(playerName.toUpperCase());
 };
 
 player1 = {
@@ -20,18 +18,28 @@ player2 = {
     health: 40
 };
 
-showPlayerName(player1.name);
-showPlayerName(player2.name);
+showPlayerName(player1.name); 
+showPlayerName(player2.name);  
+var player1;
+var player2;
+var showPlayerName;
 
+showPlayerName = function (playerName) {
+    console.log(playerName.toLowerCase());
+};
 
+player1 = {
+    name: "Kandra",
+    place: "The Dungeon of Doom",
+    health: 50
+};
 
-/* Further Adventures
- *
- * 1) In the showPlayerName function,
- *    change playerName to playerName.toUpperCase().
- *    Run the function.
- *
- * 2) Change the function to show the player's
- *    name in lower case.
- *
- */
+player2 = {
+    name: "Dax",
+    place: "The Old Library",
+    health: 40
+};
+
+showPlayerName(player1.name);  
+showPlayerName(player2.name);  
+
