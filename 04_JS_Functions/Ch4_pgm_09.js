90e0281cad6302a582e5a2313dee0b308d0c3d6a
2226j1565 <144420321+2226j1565@users.noreply.github.com>
2024-07-25 22:16:26 +0530

    Update Ch4_pgm_09.js

--- 04_JS_Functions/Ch4_pgm_09.js
+++ 04_JS_Functions/Ch4_pgm_09.js
@@ -1,33 +1,47 @@
-// Using a function to display object properties
-
 var showMovieInfo;
 
 showMovieInfo = function () {
-  console.log("Movie information for " + movie.title);
-  console.log("------------------------------");
-  console.log("Actors: " + movie.actors);
-  console.log("Directors: " + movie.directors);
-  console.log("------------------------------");
+    console.log("Movie information for " + movie.title);
+    console.log("------------------------------");
+    console.log("Actors: " + movie.actors);
+    console.log("Directors: " + movie.directors);
+    console.log("------------------------------");
 };
+showMovieInfo();
+var movie;
+var showMovieInfo;
 
+showMovieInfo = function () {
+    console.log("Movie information for " + movie.title);
+    console.log("------------------------------");
+    console.log("Actors: " + movie.actors);
+    console.log("Directors: " + movie.directors);
+    console.log("------------------------------");
+};
+showMovieInfo();
+var movie = {};
+var showMovieInfo;
 
+showMovieInfo = function () {
+    console.log("Movie information for " + movie.title);
+    console.log("------------------------------");
+    console.log("Actors: " + movie.actors);
+    console.log("Directors: " + movie.directors);
+    console.log("------------------------------");
+};
+showMovieInfo();
+var movie = {
+    title: "Inception",
+    actors: ["Leonardo DiCaprio", "Ellen Page", "Tom Hardy"],
+    directors: ["Christopher Nolan"]
+};
+var showMovieInfo;
 
-/* Further Adventures
- *
- * 1) Add a call to showMovieInfo and run the program.
- *    What happens?
- *
- * 2) Declare a movie variable
- *    but don't assign it a value.
- *    Run the program. What happens now?
- *
- * 3) Create an empty object and
- *    assign it to the movie variable.
- *    Run the program again.
- *    Does the output change?
- *
- * 4) Add title, actors and directors properties
- *    to the movie object. Run one more time.
- *    Is the output as expected?
- *
- */
+showMovieInfo = function () {
+    console.log("Movie information for " + movie.title);
+    console.log("------------------------------");
+    console.log("Actors: " + movie.actors.join(", "));
+    console.log("Directors: " + movie.directors.join(", "));
+    console.log("------------------------------");
+};
+showMovieInfo();
