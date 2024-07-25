5dc0e6ad34a095bafe2050ef0b542f9ca66c5aac
2226j1565 <144420321+2226j1565@users.noreply.github.com>
2024-07-25 22:12:02 +0530

    Update Ch4_pgm_07.js

--- 04_JS_Functions/Ch4_pgm_07.js
+++ 04_JS_Functions/Ch4_pgm_07.js
@@ -1,28 +1,25 @@
-// Using the findTotal function to display a calculation
-
-var number1 = 1000;
-var number2 = 66;
+var number1 = 500;
+var number2 = 78;
 var result;
 var findTotal;
 
 findTotal = function () {
-	result = number1 + number2;
+    result = number1 + number2;
 };
 
 findTotal();
 
 console.log(number1 + " + " + number2 + " = " + result);
+var number1 = 500;
+var number2 = 78;
+var number3 = 25;
+var result;
+var findTotal;
 
+findTotal = function () {
+    result = number1 + number2 + number3;
+};
 
+findTotal();
 
-/* Further Adventures
- *
- * 1) Change number1 and number2 and run the program
- *
- * 2) Add a third variable, number3, and assign it a value
- *
- * 3) Update the findTotal function to add the three numbers
- *
- * 4) Update the console.log line to show the new calculation
- *
- */
+console.log(number1 + " + " + number2 + " + " + number3 + " = " + result);
