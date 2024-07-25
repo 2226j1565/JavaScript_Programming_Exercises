cd63733fee146a5df136119c9bfe88a767d39edb
2226j1565 <144420321+2226j1565@users.noreply.github.com>
2024-07-25 22:59:36 +0530

    Update Ch6_pgm_01.js

--- 06_JS_Return_values/Ch6_pgm_01.js
+++ 06_JS_Return_values/Ch6_pgm_01.js
@@ -1,35 +1,10 @@
-// Returning a value from a function
-
-var getMessage;
-var response;
-
-getMessage = function () {
+var getMessage = function () {
     return "I’m going on an adventure!";
 };
 
-response = getMessage();
+var getMyMessage = function () {
+    return "Embarking on a new journey!";
+};
 
+var response = getMessage();
 console.log(response);
-
-
-
-/* Further Adventures
- *
- * 1) Write a getMyMessage function
- *    that returns a message of
- *    your choosing.
- *
- * We can call functions at the console prompt.
- * Run the program, then click in the Console panel
- * and enter commands after the prompt, >
- *
- * 2) Call getMessage at the prompt.
- *    
- *    > getMessage()
- *
- * When a function is called at the prompt,
- * its return value is displayed.
- *
- * 3) Call getMyMessage at the prompt.
- *
- */
