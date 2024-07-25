25892cc2d22ccf88a4a7788fd180c0c7a3f257f6
2226j1565 <144420321+2226j1565@users.noreply.github.com>
2024-07-25 22:14:00 +0530

    Update Ch4_pgm_08.js

--- 04_JS_Functions/Ch4_pgm_08.js
+++ 04_JS_Functions/Ch4_pgm_08.js
@@ -10,3 +10,36 @@ displayMenu = function () {
 };
 
 displayMenu();
+var displayMyMenu = function () {
+    console.log("Welcome to My Custom Menu:");
+    console.log("(1) View profile");
+    console.log("(2) Send message");
+    console.log("(3) Search contacts");
+    console.log("(4) Update settings");
+    console.log("(5) Logout");
+};
+
+displayMyMenu();
+var displayMenu;
+
+displayMenu = function () {
+    console.log("Please choose an option:");
+    console.log("(1) Print log");
+    console.log("(2) Upload file");
+    console.log("(3) Delete user");
+    console.log("(4) View settings");
+    console.log("(9) Quit");
+};
+
+var displayMyMenu = function () {
+    console.log("Welcome to My Custom Menu:");
+    console.log("(1) View profile");
+    console.log("(2) Send message");
+    console.log("(3) Search contacts");
+    console.log("(4) Update settings");
+    console.log("(5) Logout");
+};
+
+displayMenu();
+console.log(); 
+displayMyMenu();
