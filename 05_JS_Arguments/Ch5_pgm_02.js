00fa24b74b49554c48795a093501e0385e31da72
2226j1565 <144420321+2226j1565@users.noreply.github.com>
2024-07-25 22:28:03 +0530

    Update Ch5_pgm_02.js

--- 05_JS_Arguments/Ch5_pgm_02.js
+++ 05_JS_Arguments/Ch5_pgm_02.js
@@ -1,28 +1,36 @@
-// Breaking a function by changing a variable name
-
 var msg;
 var showMessage;
 
 msg = "It's full of stars!";
 
 showMessage = function () {
-	console.log(message);
+    console.log(msg); 
 };
 
-showMessage();
+showMessage(); 
+var msg;
+var newMsg;
+var showMessage;
 
+msg = "It's full of stars!";
+newMsg = "Hello, universe!";
 
+showMessage = function () {
+    console.log(msg);
+    console.log(newMsg);
+};
 
-/* Further Adventures
- *
- * 1) Update the console.log so the program works.
- *
- * 2) Declare another message variable
- *    and assign it a value.
- *
- * 3) Display the new message as well as the old one.
- *
- * 4) Display a single message created by joining
- *    the two strings.
- *
- */
+showMessage(); 
+
+var msg;
+var newMsg;
+var showMessage;
+
+msg = "It's full of stars!";
+newMsg = "Hello, universe!";
+
+showMessage = function () {
+    console.log(msg + " " + newMsg);
+};
+
+showMessage(); 
