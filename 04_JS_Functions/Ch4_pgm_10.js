7490a1a937f4bfc4026cb9d1a7a6744846e389e8
2226j1565 <144420321+2226j1565@users.noreply.github.com>
2024-07-25 22:19:26 +0530

    Update Ch4_pgm_10.js

--- 04_JS_Functions/Ch4_pgm_10.js
+++ 04_JS_Functions/Ch4_pgm_10.js
@@ -1,5 +1,3 @@
-// Calling the showMovieInfo function
-
 var movie1;
 var showMovieInfo;
 var movie;
@@ -11,33 +9,64 @@ movie1 = {
 };
 
 showMovieInfo = function () {
-  console.log("Movie information for " + movie.title);
-  console.log("------------------------------");
-  console.log("Actors: " + movie.actors);
-  console.log("Directors: " + movie.directors);
-  console.log("------------------------------");
+    console.log("Movie information for " + movie.title);
+    console.log("------------------------------");
+    console.log("Actors: " + movie.actors);
+    console.log("Directors: " + movie.directors);
+    console.log("------------------------------");
 };
 
-movie = movie1;
+
+movie = movie2; 
 
 showMovieInfo();
+var movie1;
+var showMovieInfo;
+var movie;
 
+movie1 = {
+    title: "Inside Out",
+    actors: "Amy Poehler, Bill Hader",
+    directors: "Pete Doctor, Ronaldo Del Carmen"
+};
 
+showMovieInfo = function () {
+    console.log("Movie information for " + movie.title);
+    console.log("------------------------------");
+    console.log("Actors: " + movie.actors);
+    console.log("Directors: " + movie.directors);
+    console.log("------------------------------");
+};
 
-/* Further Adventures
- *
- * 1) Without declaring a movie2 variable,
- *    assign movie2 to the movie variable
- *    instead of movie1.
- *    i.e. movie = movie2;
- *    What happens when you call showMovieInfo?
- *
- * 2) Create an empty object and
- *    assign it to a movie2 variable.
- *    What happens when you call showMovieInfo now?
- *
- * 3) Fill out movie2 with the properties
- *    needed by showMovieInfo.
- *    What happens when you call showMovieInfo now?
- *
- */
+var movie2 = {}; 
+
+movie = movie2;
+
+showMovieInfo();
+var movie1;
+var showMovieInfo;
+var movie;
+
+movie1 = {
+    title: "Inside Out",
+    actors: "Amy Poehler, Bill Hader",
+    directors: "Pete Doctor, Ronaldo Del Carmen"
+};
+
+showMovieInfo = function () {
+    console.log("Movie information for " + movie.title);
+    console.log("------------------------------");
+    console.log("Actors: " + movie.actors);
+    console.log("Directors: " + movie.directors);
+    console.log("------------------------------");
+};
+
+var movie2 = {
+    title: "The Shawshank Redemption",
+    actors: "Tim Robbins, Morgan Freeman",
+    directors: "Frank Darabont"
+};
+
+movie = movie2;
+
+showMovieInfo();
