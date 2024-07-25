16e3edfe5f44f2cd52a4b7a3faa83f264542c056
2226j1565 <144420321+2226j1565@users.noreply.github.com>
2024-07-25 23:46:50 +0530

    Update Ch8_pgm_13.js

--- 08_JS_Arrays/Ch8_pgm_13.js
+++ 08_JS_Arrays/Ch8_pgm_13.js
@@ -19,7 +19,7 @@ var spacer = {
       return longString.substr(longString.indexOf(character), length);
     },
   
-    wrap : function (text, length, character) {
+    wrap: function (text, length, character) {
       var padLength = length - text.length - 3;
       var wrapText = character + " " + text;
       wrapText += spacer.line(padLength, " ");
@@ -34,93 +34,92 @@ var spacer = {
       boxText += spacer.line(length, character) + spacer.newLine();
       return boxText;
     }
-  };
-  
-  
-  // Player display code
-  
-  var getPlayerName = function (player) {
+};
+
+
+
+var getPlayerName = function (player) {
     return player.name;
-  };
-  
-  var getPlayerHealth = function (player) {
+};
+
+var getPlayerHealth = function (player) {
     return player.name + " has health " + player.health;
-  };
-  
-  var getPlayerPlace = function (player) {
+};
+
+var getPlayerPlace = function (player) {
     return player.name + " is in " + player.place;
-  };
-  
-  var getPlayerItems = function (player) {
+};
+
+var getPlayerItems = function (player) {
     var itemsString = "Items:" + spacer.newLine();
   
-    player.items.forEach(function (item) {
-      itemsString += "   - " + item + spacer.newLine();
+    player.items.forEach(function (item, index) {
+        itemsString += " " + (index + 1) + ". " + item + spacer.newLine();
     });
   
     return itemsString;
-  };
-  
-  var getPlayerInfo = function (player, character) {
+};
+
+var getPlayerInfo = function (player, character) {
     var place = getPlayerPlace(player);
     var health = getPlayerHealth(player);
     var longest = Math.max(place.length, health.length) + 4;
-  
+
     var info = spacer.box(getPlayerName(player), longest, character);
     info += spacer.wrap(place, longest, character);
     info += spacer.newLine() + spacer.wrap(health, longest, character);
     info += spacer.newLine() + spacer.line(longest, character);
-  
+
     info += spacer.newLine();
     info += "  " + getPlayerItems(player);
     info += spacer.newLine();
     info += spacer.line(longest, character);
-  
+
     info += spacer.newLine();
-  
+
     return info;
-  };
-  
-  var showPlayerInfo = function (player, character) {
+};
+
+var showPlayerInfo = function (player, character) {
     console.log(getPlayerInfo(player, character));
-  };
-  
-  
-  // Create a player
-  
-  var player1 = {
+};
+
+
+var player1 = {
     name: "Kandra",
     place: "The Dungeon of Doom",
     health: 50,
-    items : ["a trusty lamp"]
-  };
-  
-  showPlayerInfo(player1, "=");
-  
-  player1.items.push("a rusty key");
-  
-  showPlayerInfo(player1, "*");
-  
-  
-  
-  
-  /* Further Adventures
-   *
-   * 1) Add and remove items at the prompt
-   *    using push and pop. Notice what is displayed
-   *    when push and pop are called.
-   *
-   * 2) Use the index argument for forEach
-   *    to number the items displayed. Make
-   *    the numbers start at 1.
-   *
-   * 3) Write a showItem function that accepts
-   *    a player and an itemNumber as arguments
-   *    and displays the specified player item.
-   *
-   * 4) Write an addItem function that accepts
-   *    a player and an item title as arguments
-   *    and adds the item title to the player's
-   *    item array.
-   *
-   */
+    items: ["a trusty lamp"]
+};
+
+showPlayerInfo(player1, "=");
+
+player1.items.push("a rusty key");
+showPlayerInfo(player1, "*");
+player1.items.push("a magical amulet");
+console.log("Added an item.");
+showPlayerInfo(player1, "#");
+
+player1.items.pop();
+console.log("Removed an item.");
+showPlayerInfo(player1, "#");
+
+var showItem = function (player, itemNumber) {
+    var index = itemNumber - 1;
+    if (index >= 0 && index < player.items.length) {
+        console.log("Item " + itemNumber + ": " + player.items[index]);
+    } else {
+        console.log("Item number " + itemNumber + " does not exist.");
+    }
+};
+
+showItem(player1, 2);
+
+// 4) Function to add an item
+var addItem = function (player, itemTitle) {
+    player.items.push(itemTitle);
+};
+
+addItem(player1, "a new magic wand");
+console.log("Added a new item.");
+showPlayerInfo(player1, "@");
