5bbcba489882848a3105d47ca31ef7c964b80244
2226j1565 <144420321+2226j1565@users.noreply.github.com>
2024-07-25 23:30:35 +0530

    Update Ch7_pgm_09.js

--- 07_JS_Object_Arguments/Ch7_pgm_09.js
+++ 07_JS_Object_Arguments/Ch7_pgm_09.js
@@ -1,24 +1,9 @@
-// Finding a character with indexOf
-
 var message = "We choose to go to the Moon!";
+var indexOfGo = message.indexOf("go");
+console.log("Index of 'go': " + indexOfGo); 
 
-var charIndex = message.indexOf("M");
+var indexOfChoose = message.indexOf("choose");
+console.log(message.substr(indexOfChoose, 7)); 
 
-console.log(message.substr(charIndex, 3));
-
-
-
-/* Further Adventures
- *
- * 1) Use indexOf to find the index of where the
- *    word 'go' starts in the message string.
- *
- * 2) Use indexOf and substr to display the word
- *    'choose' from the message string.
- *
- * There is also a lastIndexOf string method.
- *
- * 3) Use lastIndexOf to find the index of the
- *    'oo' in the word 'Moon' of the message string.
- *
- */
+var lastIndexOfOo = message.lastIndexOf("oo");
+console.log("Last index of 'oo': " + lastIndexOfOo);
