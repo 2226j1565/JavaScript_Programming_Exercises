638a0463aa9e14f6cf796ba66e83ccb974034717
2226j1565 <144420321+2226j1565@users.noreply.github.com>
2024-07-25 22:10:18 +0530

    Update Ch4_pgm_06.js

--- 04_JS_Functions/Ch4_pgm_06.js
+++ 04_JS_Functions/Ch4_pgm_06.js
@@ -1,27 +1,23 @@
-// Calling the sayHello function three times
-
 var sayHello;
 
 sayHello = function () {
-	console.log("Hello World!");
+    console.log("Greetings, Earthlings!");
 };
 
+sayHello(); 
 sayHello();
-sayHello();
-sayHello();
+sayHello(); 
+var sayHello;
 
+sayHello = function () {
+    console.log("Hello\nWorld!");
+};
+var printCharacters;
 
+printCharacters = function (message) {
+    for (let i = 0; i < message.length; i++) {
+        console.log(message.charAt(i));
+    }
+};
 
-/* Further Adventures
- *
- * 1) Change the message from the sayHello function.
- *
- * To add a line-break to a string, insert '\n'
- * e.g. "Line One\nLine Two"
- *
- * 2) Break the Hello World! message across two lines.
- *
- * 3) Create a function that prints the letters
- *    of "Hello World!" one by one down the page
- *
- */
+printCharacters("Hello World!");
