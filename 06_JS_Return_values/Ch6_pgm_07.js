ed633e408e1e19f3b80824a813cc01b1f17684a1
2226j1565 <144420321+2226j1565@users.noreply.github.com>
2024-07-25 23:14:39 +0530

    Update Ch6_pgm_07.js

--- 06_JS_Return_values/Ch6_pgm_07.js
+++ 06_JS_Return_values/Ch6_pgm_07.js
@@ -1,47 +1,20 @@
-// Getting a string for a player’s name
 
-var getPlayerName;
-
-getPlayerName = function (playerName) {
-    return playerName;
+var getPlayerName = function (playerName) {
+    var prefix = "====";
+    var suffix = "====";
+    return prefix + " " + playerName + " " + suffix;
 };
 
-console.log(getPlayerName("Kandra"));
+console.log(getPlayerName("Kiki"));   
+console.log(getPlayerName("Mahesha")); 
+
+console.log(""); 
 
 
+var getPlayerNameWithBorder = function (playerName) {
+    var border = "|";
+    var borderLine = border + "\n";
+    return borderLine + border + " " + playerName + " " + border + "\n" + borderLine;
+};
 
-/* Further Adventures
- *
- * 1) Update the getPlayerName function
- *    so it adds a prefix and suffix
- *    made up of the = character.
- *    The length of the prefix and suffix
- *    should be 4.
- *
- *    getPlayerName("Kiki") should return
- *    ==== Kiki ====
- *
- *    getPlayerName("Mahesha") should return
- *    ==== Mahesha ====
- *
- * 2) Update the getPlayerName function
- *    so it includes a border on the left
- *    when displaying the name.
- *
- *    getPlayerName("Jahver") should return
- *    
- *    |
- *    | Jahver
- *    |
- *
- * Note: to add a new-line character to a
- * string, use the escape sequence '\n'
- *
- * var msg = "\na\nb\n";
- * console.log(msg) would display:
- * "
- * a
- * b
- * "
- *
- */
+console.log(getPlayerNameWithBorder("Jahver"));
