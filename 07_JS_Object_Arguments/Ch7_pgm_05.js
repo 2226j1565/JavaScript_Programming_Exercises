31d818966e7c57e94fca254e41291e7d47f01594
2226j1565 <144420321+2226j1565@users.noreply.github.com>
2024-07-25 23:24:59 +0530

    Update Ch7_pgm_05.js

--- 07_JS_Object_Arguments/Ch7_pgm_05.js
+++ 07_JS_Object_Arguments/Ch7_pgm_05.js
@@ -1,5 +1,3 @@
-// Using Math.min and Math.max
-
 var showSmaller = function (num1, num2) {
     var smaller = Math.min(num1, num2);
     var larger = Math.max(num1, num2);
@@ -7,19 +5,26 @@ var showSmaller = function (num1, num2) {
     console.log(smaller + " is smaller than " + larger);
 };
 
-showSmaller(12, 3);
-showSmaller(-10, 3);
+var showLarger = function (num1, num2) {
+    var larger = Math.max(num1, num2);
+    var smaller = Math.min(num1, num2);
+
+    console.log(larger + " is larger than " + smaller);
+};
+
+var showSmallest = function (num1, num2, num3) {
+    var smallest = Math.min(num1, num2, num3);
+
+    console.log(smallest + " is the smallest of the three numbers");
+};
 
 
+showSmaller(12, 3);           
+showSmaller(-10, 3);          
 
-/* Further Adventures
- *
- * 1) Try out some different arguments.
- *
- * 2) Write a showLarger function.
- *
- * 3) Write a showSmallest function that takes
- *    three arguments and displays the smallest
- *    of the three.
- *
- */
+showLarger(12, 3);           
+showLarger(-10, 3);          
+
+showSmallest(5, 8, 3);       
+showSmallest(-1, -7, 0);     
+showSmallest(10, 10, 10);    
