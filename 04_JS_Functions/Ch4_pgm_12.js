d032a51af9dfe7722cb22be47690271c9511bc7a
2226j1565 <144420321+2226j1565@users.noreply.github.com>
2024-07-25 22:23:50 +0530

    Update Ch4_pgm_12.js

--- 04_JS_Functions/Ch4_pgm_12.js
+++ 04_JS_Functions/Ch4_pgm_12.js
@@ -1,8 +1,7 @@
-// Using functions to add and display tax
-
 var sale1;
 var sale2;
 var sale3;
+var sale4; 
 var sale;
 var calculateTax;
 var displaySale;
@@ -10,16 +9,17 @@ var displaySale;
 sale1 = { price: 140, taxRate: 15 };
 sale2 = { price: 40, taxRate: 10 };
 sale3 = { price: 120, taxRate: 20 };
+sale4 = { price: 200, taxRate: 18 }; 
 
 calculateTax = function () {
-	sale.tax = sale.price * sale.taxRate / 100;
-	sale.total = sale.price + sale.tax;
+    sale.tax = sale.price * sale.taxRate / 100;
+    sale.total = sale.price + sale.tax;
 };
 
 displaySale = function () {
-  console.log("price = $" + sale.price);
-  console.log("tax @ " + sale.taxRate + "% = $" + sale.tax);
-  console.log("total cost = $" + sale.total);
+    console.log("price = $" + sale.price);
+    console.log("tax @ " + sale.taxRate + "% = $" + sale.tax);
+    console.log("total cost = $" + sale.total);
 };
 
 sale = sale1;
@@ -34,21 +34,33 @@ sale = sale3;
 calculateTax();
 displaySale();
 
+sale = sale4;
+calculateTax();
+displaySale();
+var sales = [
+    { price: 140, taxRate: 15 },
+    { price: 40, taxRate: 10 },
+    { price: 120, taxRate: 20 },
+    { price: 200, taxRate: 18 }
+];
 
+var calculateTax = function (sale) {
+    sale.tax = sale.price * sale.taxRate / 100;
+    sale.total = sale.price + sale.tax;
+};
 
-/* Further Adventures
- *
- * 1) Add a fourth sale object.
- *
- * 2) Update the code so that tax is calculated
- *    and the new sale is displayed.
- *
- * Having two separate functions to calculate tax
- * and display sales is good, each has a specific purpose.
- * Having to call both functions
- * for each sale object is not so good.
- *
- * 3) Can you change the code so that you don't
- *    have to call both functions for every sale?
- *
- */
+var displaySale = function (sale) {
+    console.log("price = $" + sale.price);
+    console.log("tax @ " + sale.taxRate + "% = $" + sale.tax);
+    console.log("total cost = $" + sale.total);
+    console.log("------------------------------");
+};
+
+var processSale = function (sale) {
+    calculateTax(sale);
+    displaySale(sale);
+};
+
+sales.forEach(function(sale) {
+    processSale(sale);
+});
