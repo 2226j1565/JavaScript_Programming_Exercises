353607ed8630f65971f32f36f6c091c2c0a72a08
2226j1565 <144420321+2226j1565@users.noreply.github.com>
2024-07-25 22:45:23 +0530

    Update Ch5_pgm_10.js

--- 05_JS_Arguments/Ch5_pgm_10.js
+++ 05_JS_Arguments/Ch5_pgm_10.js
@@ -1,5 +1,3 @@
-// Displaying a player's health via object properties
-
 var player1;
 var player2;
 var showPlayerHealth;
@@ -11,35 +9,26 @@ showPlayerHealth = function (playerName, playerHealth) {
 player1 = {
     name: "Kandra",
     place: "The Dungeon of Doom",
-    health: 50
+    health: 50,
+    healthMultiplier: 1.2  
 };
 
 player2 = {
     name: "Dax",
     place: "The Old Library",
-    health: 40
+    health: 40,
+    healthMultiplier: 1.1  
 };
 
 showPlayerHealth(player1.name, player1.health);
 showPlayerHealth(player2.name, player2.health);
-
-
-
-/* Further Adventures
- *
- * 1) Add a new property to the player objects
- *    called healthMultiplier.
- *
- * 2) Add a third parameter to the definition
- *    of the showPlayerHealth function
- *    called playerHealthMultiplier
- *
- * 3) Update the function so that the health
- *    displayed is first multiplied by
- *    the health multiplier.
- *
- * 4) Add the player's healthMultiplier property
- *    as a third argument to the two calls
- *    to the showPlayerHealth function.
- *
- */
+showPlayerHealth = function (playerName, playerHealth, playerHealthMultiplier) {
+    var modifiedHealth = playerHealth * playerHealthMultiplier;
+    console.log(playerName + " has health " + modifiedHealth);
+};
+showPlayerHealth = function (playerName, playerHealth, playerHealthMultiplier) {
+    var modifiedHealth = playerHealth * playerHealthMultiplier;
+    console.log(playerName + " has health " + modifiedHealth);
+};
+showPlayerHealth(player1.name, player1.health, player1.healthMultiplier);
+showPlayerHealth(player2.name, player2.health, player2.healthMultiplier);
