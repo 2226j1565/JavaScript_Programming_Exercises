cdd1d1c7a72dde4f69e578858e47bf58a99f5596
2226j1565 <144420321+2226j1565@users.noreply.github.com>
2024-07-25 22:25:11 +0530

    Update Ch4_pgm_13.js

--- 04_JS_Functions/Ch4_pgm_13.js
+++ 04_JS_Functions/Ch4_pgm_13.js
@@ -1,48 +1,24 @@
-// A function to display player information
-
-var player1;
-var player2;
-var player;
-var showPlayerInfo;
-
-player1 = {
-    name: "Kandra",
-    place: "The Dungeon of Doom",
-    health: 50
+var showPlayerPlace = function (player) {
+    console.log(player.name + " is in " + player.place);
 };
 
-player2 = {
-    name: "Dax",
-    place: "The Old Library",
-    health: 40
+
+showPlayerPlace(player1);
+showPlayerPlace(player2); 
+var showPlayerHealth = function (player) {
+    console.log(player.name + " has health " + player.health);
 };
 
-showPlayerInfo = function () {
+showPlayerHealth(player1); 
+showPlayerHealth(player2); 
+var showPlayerInfo = function (player) {
     console.log(player.name);
     console.log("------------------------------");
-    console.log(player.name + " is in " + player.place);
-    console.log(player.name + " has health " + player.health);
+    showPlayerPlace(player);
+    showPlayerHealth(player);
     console.log("------------------------------");
     console.log("");
 };
 
-player = player1;
-showPlayerInfo();
-
-player = player2;
-showPlayerInfo();
-
-
-
-/* Further Adventures
- *
- * 1) Write a function that just shows
- *    where the player is.
- *
- * 2) Write a function that just shows
- *    the player's health.
- *
- * 3) Change the showPlayerInfo function
- *    to use your two functions.
- *
- */
+showPlayerInfo(player1);
+showPlayerInfo(player2);
