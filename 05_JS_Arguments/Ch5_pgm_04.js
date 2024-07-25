5b68c237c48b5f1ae971bbbd4dbf0b1ac248f912
2226j1565 <144420321+2226j1565@users.noreply.github.com>
2024-07-25 22:34:15 +0530

    Update Ch5_pgm_04.js

--- 05_JS_Arguments/Ch5_pgm_04.js
+++ 05_JS_Arguments/Ch5_pgm_04.js
@@ -1,27 +1,27 @@
-// Calling the same function with different arguments
-
 var showMessage;
 
 showMessage = function (message) {
-	console.log("The message is: " + message);
+    console.log("The message is:\n" + message);
 };
 
 showMessage("It's full of stars!");
 showMessage("Hello to Jason Isaacs");
 showMessage("Hello to Jason Isaacs and Stephen Fry");
+var showMessage;
 
+showMessage = function (message) {
+    console.log("The message is:\n" + message);
+};
 
+var myMessage = "Learning JavaScript is fun!"; 
 
-/* Further Adventures
- *
- * 1) Change the showMessage function to
- *    display its prefixed text on a
- *    separate line to the message.
- *
- * 2) Declare a myMessage variable and
- *    assign it a string value.
- *
- * 3) Call the showMessage function with
- *    myMessage as the argument.
- *
- */
+showMessage(myMessage); 
+var showMessage;
+
+showMessage = function (message) {
+    console.log("The message is:\n" + message);
+};
+
+var myMessage = "Learning JavaScript is fun!";
+
+showMessage(myMessage); 
