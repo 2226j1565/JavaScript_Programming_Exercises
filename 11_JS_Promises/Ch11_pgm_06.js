5285ebb0c5c1fc0a24860b8ceaab8e2b09ed9b70
2226j1565 <144420321+2226j1565@users.noreply.github.com>
2024-07-27 15:52:50 +0530

    Update Ch11_pgm_06.js

--- 11_JS_Promises/Ch11_pgm_06.js
+++ 11_JS_Promises/Ch11_pgm_06.js
@@ -1,4 +1,17 @@
-const countriesAPI = 'https://restcountries.com/v2/all'
-const catsAPI = 'https://api.thecatapi.com/v1/breeds'
-
-// Print out all the cat names in to catNames variable.
+const catsAPI = 'https://api.thecatapi.com/v1/breeds';
+const fetchCatNames = async () => {
+    try {
+        
+        const response = await fetch(catsAPI);
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        const catBreeds = await response.json();
+        const catNames = catBreeds.map(breed => breed.name);
+        console.log(catNames);
+        return catNames;
+    } catch (error) {
+        console.error('Error fetching cat names:', error);
+    }
+};
+fetchCatNames();
