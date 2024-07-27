9344fa25c413a58160a9da9d0989d93e5ea403a1
2226j1565 <144420321+2226j1565@users.noreply.github.com>
2024-07-27 15:02:05 +0530

    Update Ch9_pgm_06.js

--- 09_JS_Constructors/Ch9_pgm_06.js
+++ 09_JS_Constructors/Ch9_pgm_06.js
@@ -1,3 +1,4 @@
+// Define the Planet constructor function
 var Planet = function (name, position, type) {
     this.name = name;
     this.position = position;
@@ -7,48 +8,45 @@ var Planet = function (name, position, type) {
     this.showPlanet = function () {
         console.log(this.name);
         console.log("Planet " + this.position + " - " + this.type);
-        console.log("Moons: " + this.moons.join(', ') + ".");
+        this.showMoons();
     };
   
     this.addMoon = function (moon) {
         this.moons.unshift(moon);
     };
+    this.showMoons = function () {
+        if (this.moons.length > 0) {
+            this.moons.forEach(function(moon, index) {
+                console.log("(" + index + ") " + moon);
+            });
+        } else {
+            console.log("No moons.");
+        }
+    };
+    this.getMoon = function (index) {
+        if (index >= 0 && index < this.moons.length) {
+            return this.moons[index];
+        } else {
+            return "Index out of range";
+        }
+    };
 };
-
 var planet1 = new Planet("Jupiter", 5, "Gas Giant");
 planet1.addMoon("Io");
 planet1.addMoon("Europa");
+planet1.addMoon("Ganymede"); 
 
 var planet2 = new Planet("Neptune", 8, "Ice Giant");
 planet2.addMoon("Triton");
+planet2.addMoon("Nereid"); 
 
 var planet3 = new Planet("Mercury", 1, "Terrestrial");
-
+planet3.addMoon("None"); 
 [ planet1, planet2, planet3 ].forEach(function (planet) {
     planet.showPlanet();
+    console.log(); 
 });
-
-
-
-/* Further Adventures
- *
- * 1) Add a new moon to each planet.
- *
- * 2) Create a showMoons method in the
- *    Planet constructor. It should
- *    show the moons with their index.
- *
- *    (0) Io
- *    (1) Europa
- *
- * 3) Update showPlanet to use showMoons
- *    rather than join for the moons.
- *
- * 4) Create a getMoon method that
- *    returns the moon at a given
- *    index.
- *
- *    > planet1.getMoon(1)
- *      Europa
- *
- */
+console.log("Moon at index 1 of Jupiter: " + planet1.getMoon(1)); 
+console.log("Moon at index 2 of Neptune: " + planet2.getMoon(2)); 
+console.log("Moon at index 0 of Mercury: " + planet3.getMoon(0)); 
+console.log("Moon at index 1 of Mercury: " + planet3.getMoon(1)); 
