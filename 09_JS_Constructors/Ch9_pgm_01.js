a2c3c2e974de71e0b114822d922207203dce88e7
2226j1565 <144420321+2226j1565@users.noreply.github.com>
2024-07-27 14:56:06 +0530

    Update Ch9_pgm_01.js

--- 09_JS_Constructors/Ch9_pgm_01.js
+++ 09_JS_Constructors/Ch9_pgm_01.js
@@ -1,32 +1,8 @@
-// Using a function to create an object
-
-var buildPlanet = function (name, position, type) {
-    var planet = {};
-  
-    planet.name = name;
-    planet.position = position;
-    planet.type = type;
-  
-    return planet;
-};
-
-var planet1 = buildPlanet(
-    "Jupiter",
-    5,
-    "Gas Giant"
+// Build the second planet
+var planet2 = buildPlanet(
+    "Mars",
+    4,
+    "Terrestrial"
 );
-
-console.log(planet1.name);
-console.log(planet1.position);
-console.log(planet1.type);
-
-
-
-/* Further Adventures
- *
- * 1) Build a second planet,
- *    using the buildPlanet function.
- *
- * 2) Log its name and type.
- *
- */
+console.log(planet2.name); 
+console.log(planet2.type);
