e6f1a67d1f5e18cd33b973a120bb78f91c72ee03
2226j1565 <144420321+2226j1565@users.noreply.github.com>
2024-07-27 16:01:31 +0530

    Update Ch12_pgm_01.js

--- 12_JS_Sets_Maps/Ch12_pgm_01.js
+++ 12_JS_Sets_Maps/Ch12_pgm_01.js
@@ -1,11 +1,25 @@
-const a = [4, 5, 8, 9]
-const b = [3, 4, 5, 7]
-const countries = ['Finland', 'Sweden', 'Norway']
+const emptySet = new Set();
+console.log(emptySet); 
+const numbersSet = new Set();
+for (let i = 0; i <= 10; i++) {
+  numbersSet.add(i);
+}
+console.log(numbersSet); 
+const numbersSet = new Set([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+numbersSet.delete(5); 
+console.log(numbersSet); 
+const numbersSet = new Set([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+numbersSet.clear();
+console.log(numbersSet); 
+const countries = ['Finland', 'Sweden', 'Norway', 'Denmark', 'Iceland'];
+const countriesSet = new Set(countries);
+console.log(countriesSet); 
+const countries = ['Finland', 'Sweden', 'Norway'];
+const countriesMap = new Map();
 
+countries.forEach(country => {
+  countriesMap.set(country, country.length);
+});
 
-// create an empty set
-// Create a set containing 0 to 10 using loop
-// Remove an element from a set
-// Clear a set
-// Create a set of 5 string elements from array
-// Create a map of countries and number of characters of a country
+console.log(countriesMap);
+
